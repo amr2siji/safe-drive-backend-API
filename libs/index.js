08f13d2f196fd7ea35684308f57087a0c8bb2bcf
amr2siji <160729120+amr2siji@users.noreply.github.com>
2024-03-05 09:11:00 +0530

    Revert "v 1.01 - Ammar"

--- libs/index.js
+++ libs/index.js
@@ -1,7 +1,5 @@
 const app = require('./app.js');
 const db = require('../config/db')
-const UserModel = require('../models/user_model')
-
 const port = 3000;
  app.listen(port, ()=>{
      console.log('Server lisening on port http://localhost:${port}')
